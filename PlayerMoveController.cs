14d8982b4aab2915c8d1860da12b74192f537e00
Sergey <sergeykushnir12@gmail.com>
2018-10-11 11:33:11 +0300

    SampleCode

---
+++ PlayerMoveController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveController : MonoBehaviour {
+
+
+    //Public
+    public bool isMoveToEdgePosition;
+    public bool isCanMovetoEdgePosition;
+    public Vector3 edgePosVector3;
+    public Score score;
+    public BG_Instant bG_Instant;
+
+    //Private
+    private Animator animator;
+    private GameObject FirstEdgePositionGO;
+    private CreatePlatformsController createPlatformsController;
+    private CameraMove cameraMove;
+    private PogruzhikMoveController pogruzhikMoveController;
+    private GoldenCubeInstant goldenCubeInstant;
+    
+   
+
+    //Tags Variables
+    private string edgePos = "FirstEdgePosition";
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+        animator = GetComponent<Animator>();
+        FirstEdgePositionGO = GameObject.FindGameObjectWithTag(edgePos);
+        createPlatformsController = FindObjectOfType<CreatePlatformsController>();
+        cameraMove = FindObjectOfType<CameraMove>();
+        pogruzhikMoveController = FindObjectOfType<PogruzhikMoveController>();
+        goldenCubeInstant = FindObjectOfType<GoldenCubeInstant>();
+    }
+
+    private void Update()
+    {
+        //Player Start Moving
+        if (isMoveToEdgePosition)
+        {
+            print(isMoveToEdgePosition);
+            animator.SetBool("isMove", true);
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(FirstEdgePositionGO.transform.position.x, transform.position.y, transform.position.z), Time.deltaTime);
+            if (transform.position.x == FirstEdgePositionGO.transform.position.x)
+            {
+                isMoveToEdgePosition = false;
+                animator.SetBool("isMove", false);
+                createPlatformsController.InstPlat();
+                cameraMove.isCanMoveCamera = false;
+            }       
+        }
+
+        //Player Moving
+        if(isCanMovetoEdgePosition)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * 2);
+            if (transform.position.x >= edgePosVector3.x)
+            {
+                createPlatformsController.InstPlat();
+                pogruzhikMoveController.isMoveToPosRight = true;
+                cameraMove.isCanMoveCamera = true;
+                pogruzhikMoveController.isCanMovePogruzhicktoCameraCenter = true;
+                cameraMove.isResetAllVar = true;
+                goldenCubeInstant.InstantGoldenCube();
+                score.AddScore();
+                bG_Instant.isInstant = true;
+                isCanMovetoEdgePosition = false;               
+            }
+        }       
+    }
+}
---
+++ PogruzhikMoveController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PogruzhikMoveController : MonoBehaviour {
+
+    public bool isStartPosition;
+    public bool isCanMovePogruzhicktoCameraCenter;
+    public bool isMoveToPosRight;
+    public bool isMoveToPosLeft;
+    public Transform PosCenter;
+    public Transform PosLeft;
+    public Transform PosRight;
+    public GameObject Pogruzhik_sprite;
+    public Transform CameMain;
+
+    private BlockCreatorContoller blockCreatorContoller;
+    private float startPosY = 4.5f;
+    private float speedPogruzchik = 5;
+    private bool isMoveToPosCenter;
+   
+    private void Start()
+    {
+        blockCreatorContoller = FindObjectOfType<BlockCreatorContoller>();
+    }
+
+    private void Update()
+    {
+        if (isStartPosition)
+        {
+            transform.position -= new Vector3(0, 0.05f, 0);
+            if (transform.position.y <= startPosY)
+            {
+                isStartPosition = false;
+                isMoveToPosRight = true;
+                blockCreatorContoller.isCanChangeBlockScale = true;
+            }
+        }
+
+        if (isMoveToPosRight)
+        {
+            Pogruzhik_sprite.transform.position = Vector3.MoveTowards(Pogruzhik_sprite.transform.position, PosRight.position, Time.deltaTime * 2);
+            if (Pogruzhik_sprite.transform.position.x >= PosRight.position.x)
+            {
+                isMoveToPosRight = false;
+                isMoveToPosLeft = true;
+            }
+        }
+
+        if (isMoveToPosLeft)
+        {
+            Pogruzhik_sprite.transform.position = Vector3.MoveTowards(Pogruzhik_sprite.transform.position, PosLeft.position, Time.deltaTime * 2);
+            if (Pogruzhik_sprite.transform.position.x <= PosLeft.position.x)
+            {
+                isMoveToPosLeft = false;
+                isMoveToPosRight = true;
+            }
+        }
+
+        //Move Pogruzchik to Center
+        if (isCanMovePogruzhicktoCameraCenter)
+        {
+            transform.position = Vector3.Lerp(transform.position,
+                                                new Vector3(CameMain.position.x,
+                                                            transform.position.y,
+                                                            transform.position.z),
+                                                Time.deltaTime * speedPogruzchik);
+        }
+    }
+
+    public void MovePogruzhiktoStartPositionY()
+    {
+        isStartPosition = true;
+    }
+}
